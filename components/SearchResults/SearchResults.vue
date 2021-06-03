622d5c66725e1483184af4200d0c9f576032d7a5
Lars Schuitema <info@larsschuitema.nl>
2021-06-03 17:01:46 +0200

    feat(searchresults): add search results

--- components/SearchResults/SearchResults.vue
+++ components/SearchResults/SearchResults.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="container mx-auto">
-    SearchResults
+    <ul id="example-1">
+      <li class="mb-4" v-for="item in items" :key="item.message">
+        <img
+          alt="Kastanjelaan 35"
+          loading="lazy"
+          sizes="(min-width: 500px) 228px, calc(100vw - 2rem)"
+          src="https://cloud.funda.nl/valentina_media/144/136/575_720x480.jpg"
+          srcset="
+            https://cloud.funda.nl/valentina_media/144/136/575_360x240.jpg 360w,
+            https://cloud.funda.nl/valentina_media/144/136/575_180x120.jpg 180w,
+            https://cloud.funda.nl/valentina_media/144/136/575_720x480.jpg 720w
+          "
+        />
+        <div class="font-semibold text-blue-2">{{ item.address }}</div>
+        <div>{{ item.postalCode }}</div>
+        <div class="font-semibold">{{ item.price }}</div>
+        <div>{{ item.surfaceArea }}</div>
+        <div class="text-blue-2">{{ item.realtor }}</div>
+        <div>
+           <UiButton tone="primary-alt">Buy</UiButton>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import { UiButton } from '@funda/ui'
+import items from './objectData'
+
+export default {
+  components: {
+    UiButton,
+  },
+  data() {
+    return {
+      items,
+    }
+  },
+};
+</script>
